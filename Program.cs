22af9de41e98ab192f217e3bdb85d449b983bbf5
Anisha0906 <anishadas880@gmail.com>
2023-01-16 22:59:28 +0530

    [Anisha]Added:Finding maximum among three integers

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGeneric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" chooes option\n1. Find maximum among 3 integers");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    MaximumNumber maximumNumber = new MaximumNumber();
+                    int intValue = maximumNumber.PrintIntegerMaximumNumber(89,45,25);
+                    Console.WriteLine("Maximum number among these integers is " + intValue);
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
